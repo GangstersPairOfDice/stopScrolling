e609104783b1123e01dd420d59234be97ebe9d64
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2023-11-28 23:38:36 +0100

    cleaned up popup and fixed broken directory

--- extensionPopup/extensionPopup.js
+++ extensionPopup/extensionPopup.js
@@ -1,13 +1,3 @@
-const timeElement = document.getElementById("time");
-const currentTime = new Date().toLocaleTimeString();
-
-timeElement.textContent = currentTime;
-
-chrome.action.setBadgeText(
-  {
-    details: "TIME",
-  },
-  () => {
-    console.log("Finished setting badge text.");
-  }
-);
+chrome.action.setBadgeText(() => {
+  console.log("Finished setting badge text.");
+});
